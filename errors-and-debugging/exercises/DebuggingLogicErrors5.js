ca59fc11a65bc055fb35584852780c0f681107e6
abarkera4 <abarkera4@gmail.com>
2024-06-15 11:34:40 -0500

    debugging exercises completed

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -3,26 +3,44 @@
 // Refactor the code to do this. Verify that your change works by updating the console.log statements.
 
 let launchReady = false;
-let fuelLevel = 17000;
+let fuelLevel = 21000;
 let crewStatus = true;
 let computerStatus = 'green';
 
+let fuelReady = false;
+let crewReady = false;
+
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
-   launchReady = true;
+   fuelReady = true;
 } else {
    console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
+   fuelReady = false;
 }
 
 console.log("launchReady = ", launchReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+   crewReady = false;
+}
+
+if (crewReady && fuelReady) {
+   launchReady = true
+   console.log("Cleared to Launch!")
+} else {
+   launchReady = false
+   console.log("LaunchReady = ", launchReady)
+}
+
+if (launchReady) {
+   console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...")
+   console.log("Liftoff!")
+} else {
+   console.log("Launch scrubbed!")
 }
 
 console.log("launchReady = ", launchReady);
