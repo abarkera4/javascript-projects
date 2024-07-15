beb8f0ed967fe30a89745548c22bb7c6c23cf66a
abarkera4 <abarkera4@gmail.com>
2024-07-15 17:28:15 -0500

    Ch 14 Studio first pass

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,27 @@
 
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(num) {
+        let messages = [
+            { condition: num % 2 === 0 && num % 3 !== 0 && num % 5 !== 0, message: 'Launch!' },
+            { condition: num % 2 !== 0 && num % 3 === 0 && num % 5 !== 0, message: 'Code!' },
+            { condition: num % 2 !== 0 && num % 3 !== 0 && num % 5 === 0, message: 'Rocks!' },
+            { condition: num % 2 === 0 && num % 3 === 0 && num % 5 !== 0, message: 'LaunchCode!' },
+            { condition: num % 2 !== 0 && num % 3 === 0 && num % 5 === 0, message: 'Code Rocks!' },
+            { condition: num % 2 === 0 && num % 3 !== 0 && num % 5 === 0, message: 'Launch Rocks!' },
+            { condition: num % 2 === 0 && num % 3 === 0 && num % 5 === 0, message: 'LaunchCode Rocks!' }
+        ];
+
+        for (let item of messages) {
+            if (item.condition) {
+                return item.message
+            }
+        }
+        return "Rutabagas! That doesn't work."
+    }
 }
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -3,6 +3,46 @@ const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
 
+  test("Organization value should be 'nonprofit'", function(){
+    let organization = launchcode.organization;
+    expect(organization).toBe("nonprofit");
+
+  });
+
+  test("executiveDirector value should be 'Jeff'", function(){
+    let executiveDirector = launchcode.executiveDirector;
+    expect(executiveDirector).toBe("Jeff");
+
+  });
+
+  test("percentageCoolEmployees value should be 100", function(){
+    let percentageCoolEmployees = launchcode.percentageCoolEmployees;
+    expect(percentageCoolEmployees).toBe(100);
+
+  });
+
+  test("programsOffered should be correct array", function(){
+
+    let testValues = ["Web Development", "Data Analysis", "Liftoff"];
+    let programsOffered = launchcode.programsOffered;
+
+    for (let i = 0; i < testValues.length; i++) {
+      expect(programsOffered[i]).toBe(testValues[i])
+    }
+    expect(programsOffered.length).toBe(3);
+
+  });
+
+  test("launchOutput tests", function(){
+    const testArgs = [2, 3, 5, 6, 15, 10, 30, 7];
+    const testReturns = ['Launch!', 'Code!', 'Rocks!', 'LaunchCode!', 'Code Rocks!', 'Launch Rocks!', 'LaunchCode Rocks!', "Rutabagas! That doesn't work."];
+
+    let launchOutput = launchcode.launchOutput;
+    for (let i = 0; i < testArgs.length; i++) {
+      expect(launchOutput(testArgs[i])).toBe(testReturns[i])
+    }
+  });
+
   // Write your unit tests here!
   
 });
