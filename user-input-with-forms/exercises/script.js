409d905f4b261ec24b0510c9c225f7394520d69a
abarkera4 <abarkera4@gmail.com>
2024-08-07 16:47:57 -0500

    Ch 25 Exercise Completed

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,22 @@
-//Code Your Solution Below
+function init() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    let testName = document.querySelector("input[name=testName]");
+    let testDate = document.querySelector("input[name=testDate]");
+    let rocketType = document.querySelector("select[name=rocketType]");
+    let boosterCount = document.querySelector("input[name=boosterCount]");
+    let windRating = document.querySelector("input[name=windRating]:checked");
+
+    if (testName.value === "" || testDate.value === "" || rocketType.value === "" || boosterCount.value === "" || !windRating) {
+      alert("All fields are required!");
+      event.preventDefault();
+      return;
+    }
+    if (boosterCount.value < 1 || boosterCount.value > 10) {
+      alert("Booster count must be a positive number less than or equal to 10");
+      event.preventDefault();
+    }
+  });
+}
+
+window.addEventListener("load", init);
